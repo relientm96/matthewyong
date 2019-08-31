843fea7d4d99cea2079dc08c7f418b9bbfe57e27
relientm96 <matthewyfy@gmail.com>
2019-09-01 01:44:11 +1000

    UPdated html code

--- js/site.js
+++ js/site.js
@@ -1,56 +1,53 @@
 /*To Load Materialize/Website Components*/
 
-$(document).ready(function(){
+$(document).ready(function() {
+    InitWaypointAnimations();
 
-  InitWaypointAnimations();
+    //Floating Button Initialization
+    $(".fixed-action-btn").floatingActionButton({
+        direction: "top",
+        hoverEnabled: false
+    });
 
-  //Floating Button Initialization
-  $('.fixed-action-btn').floatingActionButton({
-    direction: 'top',
-    hoverEnabled: false,
-  });
-  
-  //Slider Initialization and Configurations
-  $('.slider').slider({
-		full_width: true, 
-    height: 475,
-    interval: 6000, 
-    duration: 750,
-    indicators: true,
-  });
+    //Slider Initialization and Configurations
+    $(".slider").slider({
+        full_width: true,
+        height: 475,
+        interval: 6000,
+        duration: 750,
+        indicators: true
+    });
 
-  //Tooltips addition
-  $('.tooltipped').tooltip();  
+    //Tooltips addition
+    $(".tooltipped").tooltip();
 
-  $(window).on('hashchange',function(){ 
-    if(window.location.href.includes("projectSection")) {
-      $('.slider').slider('start');
-    }
-  });
-  
-  $('.slider').ready(function(){
-    $('.slider').slider('pause');
-  });
+    $(window).on("hashchange", function() {
+        if (window.location.href.includes("projectSection")) {
+            $(".slider").slider("start");
+        }
+    });
 
-  $('#projectSection').waypoint(function() {
-    $('.slider').slider('start');
-  });
+    $(".slider").ready(function() {
+        $(".slider").slider("pause");
+    });
 
-  //Modal Initialization and Configurations  
-  $('.modal').modal({
-    inDuration: 400,
-    outDuration: 400,
-  });
+    $("#projectSection").waypoint(function() {
+        $(".slider").slider("start");
+    });
 
+    //Modal Initialization and Configurations
+    $(".modal").modal({
+        inDuration: 400,
+        outDuration: 400
+    });
 });
 
 //For Smooth Scrolling to anchor
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth',
-      });
-  });
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
 });
-
